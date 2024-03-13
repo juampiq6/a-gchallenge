cb3e1ae005234b59873136916576bec15a882d39
Juampi Q <juampiq6@gmail.com>
2024-03-13 11:38:52 +1100

    feat: implement api service as a simple provider that now can be overriden in tests fix: failing provider test

--- aandgchallenge/lib/providers/word_def_prov.dart
+++ aandgchallenge/lib/providers/word_def_prov.dart
@@ -1,24 +1,10 @@
-import 'dart:async';
-
 import 'package:aandgchallenge/models/word_response_model.dart';
-import 'package:aandgchallenge/services/api_service.dart';
+import 'package:aandgchallenge/providers/api_service_prov.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final DictionaryApiService _apiService = DictionaryApiService();
-
-final wordDefinitionProv = AsyncNotifierProvider.autoDispose
-    .family<WordDefinitionProvider, List<WordResponseModel>, String>(
-        WordDefinitionProvider.new);
-
-class WordDefinitionProvider
-    extends AutoDisposeFamilyAsyncNotifier<List<WordResponseModel>, String> {
-  @override
-  FutureOr<List<WordResponseModel>> build(String arg) async {
-    try {
-      final res = await _apiService.getDefinitions(arg);
-      return res.map((e) => WordResponseModel.fromDTO(e)).toList();
-    } catch (e) {
-      rethrow;
-    }
-  }
-}
+final wordDefinitionProv =
+    FutureProvider.family<List<WordResponseModel>, String>((ref, arg) async {
+  final apiService = ref.read(dictionaryApiServiceProv);
+  final res = await apiService.getDefinitions(arg);
+  return res.map((e) => WordResponseModel.fromDTO(e)).toList();
+});
